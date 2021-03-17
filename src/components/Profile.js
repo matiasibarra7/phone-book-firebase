7093a260c9240643c58c49051c775534fbe37fbc
Matias <matiasibarra7@gmail.com>
2021-03-16 22:12:17 -0300

    Edición y vista de datos de perfil terminado

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,12 +7,15 @@ function Profile(props) {
   const [gender, setGender] = useState('')
   const [imageFile, setImageFile] = useState('')
 
+  const [dataProfile, setDataProfile] = useState(null)
+
   const [imageProfileURL, setImageProfileURL] = useState(null)
 
   const [loadingDataProfile, setLoadingDataProfile] = useState(true)
 
   useEffect(() => {
     setLoadingDataProfile(true)
+
     const getImageProfile = async() => {
       try {
         if (props.currentUser) {
@@ -24,14 +27,30 @@ function Profile(props) {
       catch (e) {
         console.log(e);
         setImageProfileURL(null)
+        setLoadingDataProfile(false)
       }
       console.log("Efecto!");
     }
 
+    const getDataProfile = async() => {
+      if (props.currentUser) {
+        try {
+          const dataUser = await db.collection(`Users-Data`).doc(props.currentUser).get()
+          /* console.log(dataUser.data()); */
+          setDataProfile(dataUser.data())
+        }
+        catch (e) {
+          console.log(e);
+        }
+        console.log("Efecto!");
+      }
+    }
 
+    getDataProfile()
     getImageProfile()
   }, [props.currentUser])
 
+  /* Same methods outside effect */ 
   const getImageProfile = async() => {
     console.log("out the useEffect");
     try {
@@ -48,6 +67,22 @@ function Profile(props) {
     }
   }
 
+  const getDataProfile = async() => {
+    if (props.currentUser) {
+      try {
+        const dataUser = await db.collection(`Users-Data`).doc(props.currentUser).get()
+        /* console.log(dataUser.data()); */
+        setDataProfile(dataUser.data())
+      }
+      catch (e) {
+        console.log(e);
+      }
+      console.log("Efecto!");
+    }
+  }
+
+  /* End same methods outside effect */ 
+
 
   const updateProfile = async(e) => {
     e.preventDefault()
@@ -62,10 +97,13 @@ function Profile(props) {
       await db.collection(`Users-Data`).doc(props.currentUser).set(userData)
       // Esto sube  la image
       console.log("fin de carga de data");
-      await store.ref(`/images/${props.currentUser}`).put(imageFile)
-      console.log("fin de carga de img");
+      if (imageFile) {
+        await store.ref(`/images/${props.currentUser}`).put(imageFile)
+        await getImageProfile()
+        console.log("fin de carga de img");
+      }
       /* .getDownloadURL() */
-      await getImageProfile()
+      await getDataProfile()
       console.log("fin de get img");
     }
     catch {
@@ -93,9 +131,9 @@ function Profile(props) {
               <label className="form-label">Gender</label>
               <select className="form-select" aria-label="Default select example" onChange={ (e) => {setGender(e.target.value)} }>
                 <option defaultValue>Open this select menu</option>
-                <option value="m">Male</option>
-                <option value="f">Female</option>
-                <option value="u">Unicorn 🦄</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Unicorn">Unicorn 🦄</option>
               </select>
             </div>
             <div className="mb-3">
@@ -106,7 +144,7 @@ function Profile(props) {
           </form>
         </div>
         <div className="col">
-          <h4 className="text-center"> User's Details </h4>
+          {/* <h4 className="text-center"> User's Details </h4> */}
           {loadingDataProfile?
             <div className="text-center">
               <div className="spinner-border text-primary" role="status">
@@ -116,16 +154,32 @@ function Profile(props) {
 
             :
             <>
+              <div className="d-flex justify-content-center">
+
+                <div className="card" style={{width: "18rem"}}>
+                  {imageProfileURL?
+                    <img src={imageProfileURL} className="card-img-top" alt="..." />
+                    :
+                    <img src="/phone-book-firebase/images/unnamed.jpg" className="card-img-top" alt="..." />
+                  }
+            
+                  {dataProfile?
+                    <div className="card-body">
+                      <h5 className="card-title">{dataProfile.nick}</h5>
+                      <p className="card-text">{dataProfile.gender}</p>
+                    </div>
+                    :
+                    <div className="card-body">
+                      <h5 className="card-title">Unknown</h5>
+                      <p className="card-text">No data</p>
+                    </div>
+                  }
+                  
+
+                </div>
+              </div>
               {/* <button onClick={getImageProfile}>Get Profile Pic</button> */}
-              {imageProfileURL?
-                <div className="text-center">
-                    <img className="img-fluid mx-auto rounded" style={{width:"70%"}} src={imageProfileURL} alt=""/>
-                </div>
-                :
-                <div className="text-center">
-                  <img className="img-fluid mx-auto rounded" style={{width:"70%"}} src="/phone-book-firebase/images/unnamed.jpg" alt=""/>
-                </div>
-              }
+              
             </>
           }
 
